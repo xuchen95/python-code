962d2d2b31cb9b771a97ab01c352cbe191dfa613
xuchen95 <511581414@qq.com>
2019-11-05 11:20:40 +0800

    new

--- Test.py
+++ Test.py
@@ -1,19 +1,4 @@
-"""说明这是一个测试python代码"""
-def fib(max):
-    n,a,b = 0,0,1
-    while n < max:
-        yield b
-        a,b = b, a+b
-        n+=1
-    return 'done'
+def fuc():
+    print('this is function')
 
-f = fib(6)
-
-print(f)
-
-for e in f:
-    print(e)
-
-
-
-
+print(fuc())

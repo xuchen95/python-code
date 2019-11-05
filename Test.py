40ca3388fd45ae74ea02ed8e8db6b10104a91d31
xuchen95 <511581414@qq.com>
2019-11-05 10:16:33 +0800

    First commit

---
+++ Test.py
@@ -0,0 +1,19 @@
+def fib(max):
+    'add genarat'
+    n,a,b = 0,0,1
+    while n < max:
+        yield b
+        a,b = b, a+b
+        n+=1
+    return 'done'
+
+f = fib(6)
+
+print(f)
+
+for e in f:
+    print(e)
+
+
+
+
